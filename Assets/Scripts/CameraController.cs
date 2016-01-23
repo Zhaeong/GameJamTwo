657e544878c6a704f3d455d3ce722e2010bb7a17
Zhaeong <owen_zzz@hotmail.com>
2016-01-23 11:22:35 -0500

    added scene with shadows and shit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject PlayerChar;
+    public float cameraXval = 0;
+    public float cameraYval = 3;
+    public float cameraZval = -5;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPosit = PlayerChar.transform.position;
+        float cameraX = PlayerChar.transform.position.x + cameraXval;
+        float cameraY = PlayerChar.transform.position.y + cameraYval;
+        float cameraZ = PlayerChar.transform.position.z + cameraZval;
+        transform.position = new Vector3(cameraX, cameraY, cameraZ);
+        transform.LookAt(playerPosit);
+
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,20 +12,19 @@ public class PlayerController : MonoBehaviour {
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
+    public float rotateSpeed = 3.0f;
     private Vector3 moveDirection = Vector3.zero;
     void Update()
     {
-        CharacterController controller = GetComponent<CharacterController>();
-        if (controller.isGrounded)
-        {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed;
 
-        }
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * curSpeed);
     }
 }
